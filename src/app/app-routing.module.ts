23ff9d860b71ca0f0bbca4f4c0d5f34671a9b6ff
VictorPrado002 <victor000hugo10@gmail.com>
2023-11-07 23:38:37 -0600

    implementacion de nuevos modulos en front

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { ReservacionesComponent } from './reservaciones/reservaciones.component';
+import { PaginaLoginComponent } from './pagina-login/pagina-login.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home",component:HomeComponent},
+  {path:"menu",component:MenuComponent},
+  {path:"reservacion",component:ReservacionesComponent},
+  {path:"about",component:AboutComponent},
+  {path:"loginPag",component:PaginaLoginComponent},
+  {path:"**", pathMatch:"full",redirectTo:"home"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
