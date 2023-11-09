2f97d4c459e376d5af9d5d46b04fa8a58fa73ffd
AngelFs.exe <113188236+NotAngelexe@users.noreply.github.com>
2023-11-08 23:11:33 -0600

    Espero que ya se suba esta chingadera xd
    Estoy harto de vivir

--- src/app/reservaciones/reservaciones.component.ts
+++ src/app/reservaciones/reservaciones.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-reservaciones',
   templateUrl: './reservaciones.component.html',
   styleUrls: ['./reservaciones.component.css']
 })
 export class ReservacionesComponent {
-
+    reservation: any = {};
+  
+    submitReservation() {
+      // Aquí se puede usar servicio para enviar la solicitud HTTP al server
+      console.log('Reservación enviada:', this.reservation);
+    }
 }
