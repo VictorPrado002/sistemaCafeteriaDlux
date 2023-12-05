56c62de7f7b9e34343abd1bdeff69c9f6b47ec36
Gonzalo263614 <gonzalo_ruso19@outlook.com>
2023-12-05 17:50:15 -0600

    A

--- src/app/reservaciones/reservaciones.component.ts
+++ src/app/reservaciones/reservaciones.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-reservaciones',
@@ -7,10 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./reservaciones.component.css']
 })
 export class ReservacionesComponent {
-    reservation: any = {};
-  
-    submitReservation() {
-      // Aquí se puede usar servicio para enviar la solicitud HTTP al server
+  reservation: any = {};
+
+  // Custom validator function for the client's name
+  validateName(name: string): boolean {
+    const regex = /^[a-zA-Z\s]+$/; // Allows only alphabets and spaces
+    return regex.test(name);
+  }
+
+  // Custom validator function for the phone number
+  validatePhoneNumber(phoneNumber: string): boolean {
+    // You can customize the phone number validation logic based on your requirements
+    // For example, you might want to ensure a specific format or length
+    const regex = /^[0-9]{10}$/; // Assuming a 10-digit phone number
+    return regex.test(phoneNumber);
+  }
+
+  // Custom validator function for the email
+  validateEmail(email: string): boolean {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Basic email format validation
+    return emailRegex.test(email);
+  }
+
+  // Custom validator function for the number of persons
+  validateNumberOfPersons(persons: number): boolean {
+    return persons >= 1 && persons <= 10;
+  }
+
+  submitReservation() {
+    if (
+      this.validateName(this.reservation.nombre) &&
+      this.validatePhoneNumber(this.reservation.telefono) &&
+      this.validateEmail(this.reservation.email) &&
+      this.validateNumberOfPersons(this.reservation.personas)
+    ) {
+      // Valid name, phone number, email, and number of persons, proceed with the reservation submission logic
+      // Aquí se puede usar un servicio para enviar la solicitud HTTP al servidor
       console.log('Reservación enviada:', this.reservation);
+    } else {
+      // Invalid input, show an error message or handle accordingly
+      console.log('Invalid input');
     }
+  }
 }
